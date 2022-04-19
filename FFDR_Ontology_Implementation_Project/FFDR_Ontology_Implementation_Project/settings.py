83f486997e06d6d98f46cbc94203f8a90962c1fb
Eyosias24 <eyosias24@gmail.com>
2022-04-19 18:17:50 +0200

    Changed debug to false

--- FFDR_Ontology_Implementation_Project/FFDR_Ontology_Implementation_Project/settings.py
+++ FFDR_Ontology_Implementation_Project/FFDR_Ontology_Implementation_Project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!b-t3$xp)1m^j7p9m-w%ilyn2skcpf)byi5bl!-3_urp#*$hi5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
