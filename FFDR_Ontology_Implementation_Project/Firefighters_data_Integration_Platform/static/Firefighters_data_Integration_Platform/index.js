9d1498d86a10452e16dcd2080d1b5dab71e9e7a9
Eyosias24 <eyosias24@gmail.com>
2022-05-11 16:32:19 +0300

    Configured search bar

--- FFDR_Ontology_Implementation_Project/Firefighters_data_Integration_Platform/static/Firefighters_data_Integration_Platform/index.js
+++ FFDR_Ontology_Implementation_Project/Firefighters_data_Integration_Platform/static/Firefighters_data_Integration_Platform/index.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
+
   // Project title
   projectTitle = "Sample Project 01";
 
@@ -380,7 +381,28 @@ document
     });
 
   document.querySelector("#incident-building").click();
+
+
+  
+
+  // Search button
+  searchButton = document.querySelector("#search-form button")
+  searchButton.addEventListener("click", function (event) {
+    // Get search item
+    searchItem = document.querySelector("#search-form input").value
+
+    searchForm(searchItem)
+
+    });
 });
+
+function searchForm(searchItem){
+
+    if(allNavItems.includes(searchItem)){
+      searchItem = searchItem.toLowerCase().split(" ").join("-");
+  document.querySelector("#" + searchItem).click();
+    }
+}
 
 function changeActiveButton(newActiveButton) {
   currentActiveButton = document.getElementsByClassName("button active");
@@ -405,6 +427,7 @@ function loadNavPanels() {
     "Control Panels"
   ];
   //Building components other than safety systems
+  
   rightNavItems = [
     "Building Utility Systems",
     "Facade",
@@ -415,10 +438,6 @@ function loadNavPanels() {
     "Concealed Spaces",
     "Vertical Openings",
     "Roof Top Element",
-    "Extremely Valuable Materials",
-    "Helipad",
-  ];
-  moreRightNavItems = [
     "Extremely Valuable Materials",
     "Helipad",
   ];
@@ -442,6 +461,7 @@ function loadNavPanels() {
     "Pipeline",
     "PowerLine",
   ];
+
   // Building safety systems on the left panel
   leftNavItems = [
     "Automatic Fire Extinguishing System",
@@ -460,14 +480,14 @@ function loadNavPanels() {
     "Fire Pump",
     "Mass Notification System",
   ];
-  moreLeftNavItems = [
-    "FARS Fill Stations",
-    "Emergency Power Outlets",
-    "Smoke and Heat Removal System",
-    "Smoke Control System",
-    "Fire Pump",
-    "Mass Notification System",
-  ];
+// List of all Navigation items
+allNavItems = [...topNavItems , ...rightNavItems , ...bottomNavItems , ...moreBottomNavItems , ...leftNavItems]
+
+
+
+  autocomplete(document.getElementById("search-input"), allNavItems);
+
+
   // Add heading to navigation panels
   // Right navigation panel
   var headingItem = document.createElement("p");
@@ -539,3 +559,115 @@ function weatherCondition() {
 }
 var buildingCoordinate = [];
 var buildingAddress;
+
+
+function autocomplete(inp, arr) {
+  /*the autocomplete function takes two arguments,
+  the text field element and an array of possible autocompleted values:*/
+  var currentFocus;
+  /*execute a function when someone writes in the text field:*/
+  inp.addEventListener("input", function(e) {
+      var a, b, i, val = this.value;
+      /*close any already open lists of autocompleted values*/
+      closeAllLists();
+      if (!val) { return false;}
+      currentFocus = -1;
+      /*create a DIV element that will contain the items (values):*/
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "search-auto-complete-list");
+      a.setAttribute("class", "search-auto-complete-items");
+      /*append the DIV element as a child of the autocomplete container:*/
+      this.parentNode.appendChild(a);
+      /*for each item in the array...*/
+      for (i = 0; i < arr.length; i++) {
+        /*check if the item starts with the same letters as the text field value:*/
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          /*create a DIV element for each matching element:*/
+          b = document.createElement("DIV");
+          /*make the matching letters bold:*/
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+          /*insert a input field that will hold the current array item's value:*/
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          /*execute a function when someone clicks on the item value (DIV element):*/
+              b.addEventListener("click", function(e) {
+              /*insert the value for the autocomplete text field:*/
+              inp.value = this.getElementsByTagName("input")[0].value;
+              /*close the list of autocompleted values,
+              (or any other open lists of autocompleted values:*/
+              closeAllLists();
+          });
+
+          a.appendChild(b);
+
+    // Get search item
+    searchItem = b.querySelector("input").value
+
+          // Click on item
+           b.addEventListener("click",  searchForm.bind(this,searchItem) )
+    //   b.addEventListener("click", function (event) {
+    //     console.log(b)
+        
+
+    // searchForm(searchItem)
+    // });
+
+        }
+      }
+  });
+  /*execute a function presses a key on the keyboard:*/
+  inp.addEventListener("keydown", function(e) {
+      var x = document.getElementById(this.id + "search-auto-complete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+        /*If the arrow DOWN key is pressed,
+        increase the currentFocus variable:*/
+        currentFocus++;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 38) { //up
+        /*If the arrow UP key is pressed,
+        decrease the currentFocus variable:*/
+        currentFocus--;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        /*If the ENTER key is pressed, prevent the form from being submitted,*/
+        e.preventDefault();
+        if (currentFocus > -1) {
+          /*and simulate a click on the "active" item:*/
+          if (x) x[currentFocus].click();
+        }
+      }
+  });
+  function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("search-auto-complete-active");
+  }
+  function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("search-auto-complete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    /*close all autocomplete lists in the document,
+    except the one passed as an argument:*/
+    var x = document.getElementsByClassName("search-auto-complete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+      x[i].parentNode.removeChild(x[i]);
+    }
+  }
+}
+/*execute a function when someone clicks in the document:*/
+document.addEventListener("click", function (e) {
+    closeAllLists(e.target);
+});
+}
