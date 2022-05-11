fd026a04002d8f036769aeb982ff6fd9701e4e1a
Eyosias24 <eyosias24@gmail.com>
2022-05-11 18:17:51 +0300

    update search function to enable it to use two words

--- FFDR_Ontology_Implementation_Project/Firefighters_data_Integration_Platform/static/Firefighters_data_Integration_Platform/index.js
+++ FFDR_Ontology_Implementation_Project/Firefighters_data_Integration_Platform/static/Firefighters_data_Integration_Platform/index.js
@@ -580,18 +580,18 @@ function autocomplete(inp, arr) {
       this.parentNode.appendChild(a);
       /*for each item in the array...*/
       for (i = 0; i < arr.length; i++) {
-
+firstIteration = true
         // Split words
         splitItemList = arr[i].split(' ')
 
         for(k = 0; k < splitItemList.length; k++)
         {
 splitItem = splitItemList[k]
-          if(splitItem != "And" || splitItem != "and" || splitItem != "")
+          if(splitItem != "And" || splitItem != "and")
           {
             
         /*check if the item starts with the same letters as the text field value:*/
-        if (splitItem.substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+        if (splitItem.substr(0, val.length).toUpperCase() == val.toUpperCase().trim()) {
           /*create a DIV element for each matching element:*/
           b = document.createElement("DIV");
           /*make the matching letters bold:*/
@@ -629,6 +629,42 @@ b.innerHTML =foundItem.join("<strong>" + splitItem.substr(0, val.length) + "</st
     // searchForm(searchItem)
     // });
 
+firstIteration = false
+        }
+
+        else if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          if(!firstIteration)
+          continue
+          /*create a DIV element for each matching element:*/
+          b = document.createElement("DIV");
+          /*make the matching letters bold:*/
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+          /*insert a input field that will hold the current array item's value:*/
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          /*execute a function when someone clicks on the item value (DIV element):*/
+              b.addEventListener("click", function(e) {
+              /*insert the value for the autocomplete text field:*/
+              inp.value = this.getElementsByTagName("input")[0].value;
+              /*close the list of autocompleted values,
+              (or any other open lists of autocompleted values:*/
+              closeAllLists();
+          });
+
+          a.appendChild(b);
+
+    // Get search item
+    searchItem = b.querySelector("input").value
+
+          // Click on item
+           b.addEventListener("click",  searchForm.bind(this,searchItem) )
+    //   b.addEventListener("click", function (event) {
+    //     console.log(b)
+        
+
+    // searchForm(searchItem)
+    // });
+firstIteration = false
         }
           }
         }
